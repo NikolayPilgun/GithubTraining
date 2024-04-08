aff253ac86d21f1ade75010e625b45d575c5ddf7
Nikolay Pilgun <a03198814s@gmail.com>
2024-04-08 18:52:23 +0500

    Made the basic markup for the footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,12 @@
 function Footer() {
-	return <footer>I change the file on github and pull the project</footer>;
+	return (
+		<footer className="grid  grid-rows-[100px] grid-cols-[minmax(100px,_300px)_minmax(0px,_1fr)]">
+			<div className="w-full h-full bg-slate-800 flex justify-center items-center">
+				<h2 className="text-xl text-cyan-50 ">Footer</h2>
+			</div>
+			<div className=""></div>
+		</footer>
+	);
 }
 
 export default Footer;
