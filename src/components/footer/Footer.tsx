a78c1a732c9994519c3b620280cf9565c66bde07
Nikolay Pilgun <a03198814s@gmail.com>
2024-04-08 19:33:30 +0500

    Footer adjustment

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,8 +4,7 @@ function Footer() {
 			<div className="w-full h-full bg-slate-800 flex justify-center items-center">
 				<h2 className="text-xl text-cyan-50 ">Footer</h2>
 			</div>
-			<div>тестовая надпись номер один сделанная в текстовом редакторе</div>
-			<div className="text-slate-100">тестовая запись сделанная на сервере</div>
+			<div></div>
 		</footer>
 	);
 }
